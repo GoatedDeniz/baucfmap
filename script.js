070f89c8a1d47a9cd02e8892e9fb3491f927eef2
GoatedDeniz <denizmak01@gmail.com>
2025-03-12 12:21:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const classrooms = {
 
 // Function to show the selected floor
 function showFloor(floor) {
-    document.getElementById("floorMap").src = `floor-${floor}.svg`;
+    document.getElementById("floorMap").src = `https://baucfclassrooms.com/floor-${floor}.svg`;
 
     // Remove "active" class from all buttons
     document.querySelectorAll(".floor-selector button").forEach(button => button.classList.remove("active"));
